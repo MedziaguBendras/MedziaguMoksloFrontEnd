4e9496ac90b03f746fc1962056371b8a6185d5a9
MedziaguBendras <57392978+MedziaguBendras@users.noreply.github.com>
2019-12-03 15:33:17 +0200

    Pradinis projekto sukūrimas

---
+++ Models/MaterialForDropDownList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MedziaguMoksloFrontEnd.Models
+{
+    public class MaterialForDropDownList
+    {
+        public Guid MaterialId { get; set; }
+        public string MaterialName { get; set; }
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using MedziaguMoksloFrontEnd.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace MedziaguMoksloFrontEnd.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public List<MaterialForDropDownList> list = new List<MaterialForDropDownList>();
+        public Rezultatas r = new Rezultatas();
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public async Task<List<MaterialForDropDownList>> GetMaterialsAsync()
+        {
+            HttpClient client = new HttpClient();
+            var responseString = await client.GetAsync("https://localhost:5001/api/material/dropdown");
+            List<MaterialForDropDownList> res = (List<MaterialForDropDownList>)JsonConvert.DeserializeObject(
+                await responseString.Content.ReadAsStringAsync(), typeof(List<MaterialForDropDownList>));
+            return res;
+        }
+    }
+    public class Rezultatas
+    {
+        public string rezultatas { get; set; }
+    }
+}
